2f9adce817c58eefe2ab4e0ecba3d34d1eeac195
massarraa05 <119047627+massarraa05@users.noreply.github.com>
2024-10-20 18:31:04 +0100

    created sign up compoent the form input compoent ,button compoent

--- src/compoents/routes/sign-in/sign-in.compoent.jsx
+++ src/compoents/routes/sign-in/sign-in.compoent.jsx
@@ -1,5 +1,6 @@
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
   } 
  from '../../../utils/firebase/utils.firebase';
@@ -9,11 +10,16 @@ import {
       const { user } = await signInWithGooglePopup();
       const userDocRef = await createUserDocumentFromAuth(user);
     };
-  
+    const logGoogleRedirectUser = async () => {
+      const { user } = await signInWithGoogleRedirect();
+      console.log({user});
+    };
     return (
       <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <button onClick={logGoogleUser}>Sign in with Google Redirect</button>
+
       </div>
     );
   };
--- src/utils/firebase/utils.firebase.js
+++ src/utils/firebase/utils.firebase.js
@@ -36,6 +36,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async (
   userAuth,
